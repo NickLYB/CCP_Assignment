ff6a532b634c9026e436028b5c9d1925dcbee9aa
Low Yi Bo <nicklowyb@gmail.com>
2025-12-19 13:16:54 +0800

    test

---
+++ src/concurrent/Concurrent.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package concurrent;
+
+/**
+ *
+ * @author NICK
+ */
+public class Concurrent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
